16c9563a75792aba7ccc0d979f579d64dc0140c1
Piotr Kasprzyk <ciri@ciri.pl>
2019-10-30 12:48:37 +0100

    Add optional text to display

--- common_rg_bar.py
+++ common_rg_bar.py
@@ -4,6 +4,7 @@
 Given:
 1. status code: (0 - OK, other value - BAD)
 2. terminal window width
+3. (optional) Text to display (without color)
 
 shows red/green bar to visualize return code of previous command
 '''
@@ -27,13 +28,18 @@ def main():
                 else:
                     col_char = '2'
             cols_limit = int(sys.argv[2])
+        if len(sys.argv) >= 4:
+            start_text = sys.argv[3] + ' '
+        else:
+            start_text = ''
         esc = chr(27)
         print (''.join((
+            start_text,
             esc,
             '[4',
             col_char,
             'm',
-            ' ' * (cols_limit - 2 - len(code)),
+            ' ' * (cols_limit - 2 - len(code) - len(start_text)),
             code,
             esc,
             '[0m',
@@ -44,6 +50,7 @@ def main():
 
     1. status code: 0 - OK (green color), other values - BAD (red color)
     2. number of columns: the width of text console
+    3. (optional) Text to display
     ''' % dict(
             prog_name=sys.argv[0],
             ))
